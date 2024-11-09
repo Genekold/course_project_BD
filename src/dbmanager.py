3f6097327063737a741e26ca5ac5a8df50dfae87
mihaylovea <mihailovevgenya@gmail.com>
2024-11-09 07:31:44 +0300

    Добавлен интерфейс пользователя

--- src/dbmanager.py
+++ src/dbmanager.py
@@ -14,13 +14,16 @@ class DBManager:
 
     def get_companies_and_vacancies_count(self) -> list[tuple]:
         """Функция получает список всех компаний и количество вакансий у каждой компании."""
+        print("1")
 
-        self.cur.execute("""
-            SELECT employers.name_employers, COUNT(vacancies.name_vacancies) 
-            FROM employers INNER JOIN vacancies 
+        self.cur.execute(
+            """
+            SELECT employers.name_employers, COUNT(vacancies.name_vacancies)
+            FROM employers INNER JOIN vacancies
             USING (employer_id)
             GROUP BY employers.name_employers
-        """)
+        """
+        )
         data = self.cur.fetchall()
 
         return data
@@ -30,11 +33,13 @@ class DBManager:
         Функция получает список всех вакансий с указанием названия компании,
         названия вакансии и зарплаты и ссылки на вакансию.
         """
-        self.cur.execute("""
-            SELECT employers.name_employers, vacancies.name_vacancies, vacancies.vacancies_url 
-            FROM vacancies INNER JOIN employers 
+        self.cur.execute(
+            """
+            SELECT employers.name_employers, vacancies.name_vacancies, vacancies.vacancies_url
+            FROM vacancies INNER JOIN employers
             USING (employer_id)
-        """)
+        """
+        )
         data = self.cur.fetchall()
 
         return data
@@ -43,11 +48,13 @@ class DBManager:
         """
         Функция получает среднюю зарплату по всем вакансиям.
         """
-        self.cur.execute("""
+        self.cur.execute(
+            """
             SELECT SUM(salary_avg)/ COUNT(salary_avg)
             FROM vacancies
             WHERE salary_avg != 0
-        """)
+        """
+        )
         data = self.cur.fetchall()
 
         return data
@@ -58,11 +65,13 @@ class DBManager:
         """
         avg_salary = round(self.get_avg_salary()[0][0], 1)
 
-        self.cur.execute(f"""
+        self.cur.execute(
+            f"""
                     SELECT name_vacancies, salary_avg, vacancies_url
                     FROM vacancies
                     WHERE salary_avg >= {avg_salary}
-                """)
+                """
+        )
         data = self.cur.fetchall()
 
         return data
@@ -70,15 +79,17 @@ class DBManager:
     def get_vacancies_with_keyword(self, keyword) -> list[tuple]:
         """Функция получает список всех вакансий, в названии которых содержатся переданные в метод слова"""
 
-        self.cur.execute(f"""
+        self.cur.execute(
+            f"""
                     SELECT name_vacancies, salary_avg, vacancies_url FROM vacancies
                     WHERE LOWER(name_vacancies) LIKE '%{keyword}%'
-                """)
+                """
+        )
         data = self.cur.fetchall()
 
         return data
 
     def exit_db(self):
-        """Функция закрывает базу данных """
+        """Функция закрывает базу данных"""
         self.cur.close()
         self.conn.close()
--- src/utils.py
+++ src/utils.py
@@ -1,29 +1,21 @@
-import os
-import psycopg2
 from typing import Any
 
-from dotenv import load_dotenv
+import psycopg2
 
 from src.hh_api import HHCompany
-
-load_dotenv()
-list_id_emp = os.getenv('LIST_ID_EMP')
 
 
 def get_employer_data(employer_ids: str) -> list[dict[str, Any]]:
     """функция получает данные о работодателях и их вакансияях"""
 
     data = []
-    for employer_id in employer_ids.split(','):
+    for employer_id in employer_ids.split(","):
         employer = HHCompany()
         employer_info = employer.employer_information(int(employer_id))
 
         open_vacancies = employer.open_vacancies(int(employer_id))
 
-        data.append({
-            'employer': employer_info,
-            'vacansy': open_vacancies
-        })
+        data.append({"employer": employer_info, "vacansy": open_vacancies})
 
     return data
 
@@ -31,71 +23,86 @@ def get_employer_data(employer_ids: str) -> list[dict[str, Any]]:
 def create_database(database_name: str, params: dict) -> None:
     """Функция создает базу данных создает таблицы 'employers' и 'vacancies'"""
 
-    conn = psycopg2.connect(dbname='postgres', **params)
+    conn = psycopg2.connect(dbname="postgres", **params)
     conn.autocommit = True
     cur = conn.cursor()
 
-    cur.execute(f'DROP DATABASE IF EXISTS {database_name}')
-    cur.execute(f'CREATE DATABASE {database_name}')
+    cur.execute(f"DROP DATABASE IF EXISTS {database_name}")
+    cur.execute(f"CREATE DATABASE {database_name}")
 
     cur.close()
     conn.close()
 
     conn = psycopg2.connect(dbname=database_name, **params)
     with conn.cursor() as cur:
-        cur.execute("""
-            CREATE TABLE employers (                
+        cur.execute(
+            """
+            CREATE TABLE employers (
                 employer_id SERIAL PRIMARY KEY,
                 name_employers VARCHAR NOT NULL,
                 description TEXT,
                 employers_url TEXT,
-                city VARCHAR,               
+                city VARCHAR,
                 open_vacancies INTEGER
             )
-        """)
+        """
+        )
 
     with conn.cursor() as cur:
-        cur.execute("""
+        cur.execute(
+            """
             CREATE TABLE vacancies (
                 name_vacancies VARCHAR,
                 vacancies_url TEXT,
                 salary_from INTEGER,
                 salary_to INTEGER,
                 salary_avg REAL,
-                employer_id INTEGER REFERENCES employers (employer_id),                
+                employer_id INTEGER REFERENCES employers (employer_id),
                 requirement TEXT,
                 responsibility TEXT,
-                contacts TEXT            
+                contacts TEXT
                )
-           """)
+           """
+        )
 
     conn.commit()
     conn.close()
 
 
-def save_data_to_database(data: list[dict[str, Any]], database_name: str, params: dict) -> None:
+def save_data_to_database(
+        data: list[dict[str, Any]], database_name: str, params: dict
+) -> None:
     """Функция сохраняет данные о компаниях и вакансиях в базу данных"""
 
     conn = psycopg2.connect(dbname=database_name, **params)
 
     with conn.cursor() as cur:
         for employer in data:
-            employer_info = employer['employer']
+            employer_info = employer["employer"]
             cur.execute(
                 """
                 INSERT INTO employers (name_employers, description, employers_url, city, open_vacancies)
                 VALUES (%s, %s, %s, %s, %s)
                 RETURNING employer_id
                 """,
-                (employer_info['name'], employer_info['description'], employer_info['site_url'],
-                 employer_info['area']['name'], employer_info['open_vacancies'])
+                (
+                    employer_info["name"],
+                    employer_info["description"],
+                    employer_info["site_url"],
+                    employer_info["area"]["name"],
+                    employer_info["open_vacancies"],
+                ),
             )
             employer_id = cur.fetchone()[0]
-            vacancies_data = employer['vacansy']
+            vacancies_data = employer["vacansy"]
             for vacancy in vacancies_data:
-                if vacancy['salary']:
-                    salary_from = vacancy['salary']['from'] if vacancy['salary']['from'] else 0
-                    salary_to = vacancy['salary']['to'] if vacancy['salary']['to'] else 0
+                if vacancy["salary"]:
+                    salary_from = (
+                        vacancy["salary"]["from"] if vacancy["salary"]["from"] else 0
+                    )
+                    salary_to = (
+                        vacancy["salary"]["to"] if vacancy["salary"]["to"] else 0
+                    )
                 else:
                     salary_from = 0
                     salary_to = 0
@@ -107,16 +114,24 @@ def save_data_to_database(data: list[dict[str, Any]], database_name: str, params
                 else:
                     salary_avg = 0
 
-                snippet = vacancy['snippet']
+                snippet = vacancy["snippet"]
 
                 cur.execute(
                     """
-                    INSERT INTO vacancies (employer_id, name_vacancies, salary_from, salary_to, salary_avg, 
+                    INSERT INTO vacancies (employer_id, name_vacancies, salary_from, salary_to, salary_avg,
                     vacancies_url, requirement, responsibility)
                     VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
                     """,
-                    (employer_id, vacancy['name'], salary_from, salary_to, salary_avg, vacancy['alternate_url'],
-                     snippet['requirement'], snippet['responsibility'])
+                    (
+                        employer_id,
+                        vacancy["name"],
+                        salary_from,
+                        salary_to,
+                        salary_avg,
+                        vacancy["alternate_url"],
+                        snippet["requirement"],
+                        snippet["responsibility"],
+                    ),
                 )
 
     conn.commit()
